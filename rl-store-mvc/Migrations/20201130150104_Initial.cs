8b660e831d834c9138cc7c75b4ff16bcce5bc276
Diego Sumiya <diego.sumiya@gmail.com>
2020-11-30 12:19:51 -0300

    Adaptação do MySql e a primeira migração

---
+++ rl-store-mvc/Migrations/20201130150104_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesManagementMVC.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CustomerId = table.Column<string>(nullable: true),
+                    Status = table.Column<int>(nullable: false),
+                    Total = table.Column<double>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Updated = table.Column<DateTime>(nullable: false),
+                    PaymentForm = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Order");
+        }
+    }
+}
--- rl-store-mvc/Startup.cs
+++ rl-store-mvc/Startup.cs
@@ -37,7 +37,8 @@ namespace rl_store_mvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesManagementMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesManagementMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesManagementMVCContext"), Builder =>
+                    Builder.MigrationsAssembly("SalesManagementMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
