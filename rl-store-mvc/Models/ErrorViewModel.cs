0186c33ce988204c5283d63d761c2aedf738f352
Diego Sumiya <diego.sumiya@gmail.com>
2020-11-30 09:18:36 -0300

    Projeto criado

---
+++ rl-store-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace rl_store_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
